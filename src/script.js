0476b44d3bdaee30b3b9989160b022300bf45ce2
a.zagoveliev <63207977+mexell96@users.noreply.github.com>
2020-05-07 11:41:24 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,26 +1,4 @@
-1)
-есть массив фруктов типа
-[{name: ‘banana’, count: 10}]
-придумайте хотя бы 5 фруктов
-
-задача проста — написать функцию, которая принимает 2 аргумента — массив и строку. 
-function f (arr, paramName) {}
-
-Пробегается по массиву и возвращает массив тех значений объекта, 
-которые указаны вторым аргументом. Использовать функцию так, 
-чтобы она вернула названия фруктов (испольуем map)
-
-на вход
-[
-{name: ‘banana’, count: 10},
-{name: ‘orange’, count: 12}
-]
-
-если мы функцию вызываем как f(arr, 'name'), на выходе [‘banana’, ‘orange’]
-
-если f(arr, ‘count’), то будет [10, 12]
-
-
+// Задание №1
 'use strict';
 let fruits = [
     {name: "banana", 
@@ -40,43 +18,43 @@ let fruits = [
 },
 ];
 
-// Первый вариант
-// let newFruit = fruits.map(fruit => {
-//     return fruit.name
-// })
-// console.log(newFruit);
-
-//  Второй вариант 
-let newFruit2 = fruits.map(function(fruits) {
-    console.log (fruits.name)
+let newFruit = fruits.map(fruit => {
+    return fruit.name
 })
+console.log(newFruit);
 
 
-2)
-есть массив людей типа
-[{name: ‘Anna’, books: [‘Harry Potter’, ‘War and Peace’]}]
-придумайте хотя бы 5 людей с любимыми книгами
-
-написать функцию, которая будет принимать массив первым аргументом. 
-На выходе получается список всех книг
-function f (arr) {} (испольуем reduce)
-
-Задание со звёздочкой — сделать так, чтобы в массиве на выходе не было дубликатов
-
-
+// Задание №2
+'use strict';
 let readers = [
     {name: "Anna", books: ["Harry Potter", "War and Peace"]},
     {name: "Oleg", books: ["Harry Potter two", "War and Peace two"]},
-    {name: "Igor", books: ["Harry Potter three", "War and Peace three"]},
-    {name: "Dasha", books: ["Harry Potter four", "War and Peace four"]},
+    {name: "Igor", books: ["Harry Potter two", "War and Peace three"]},
+    {name: "Dasha", books: ["Harry Potter two", "War and Peace four"]},
     {name: "Masha", books: ["Harry Potter five", "War and Peace five"]},
+    {name: "Maksim", books: ["Harry Potter six", "War and Peace six"]},
+    {name: "Nikita", books: ["Harry Potter seven", "War and Peace seven"]},
 ];
 
-let allBooks = readers.reduce(function(sum, elem) {
-    return sum.concat(elem.books);
+let allBooks = readers.reduce(function(acc, book) {
+    return acc.concat(book.books);
 }, []);
-console.log(allBooks);
+// console.log(allBooks);
+
+// 1.
+let newAllBooks = allBooks.filter((item,index) => {
+    return index === allBooks.indexOf(item);
+});
+console.log(newAllBooks);
 
 
+// 2.
+let newAllBooks2 = Array.from(new Set(allBooks));
+console.log(newAllBooks2);
 
 
+// 3.
+let newAllBooks3 = allBooks.reduce((uniq, item) => {
+    return uniq.includes(item) ? uniq : [...uniq, item];
+}, [])
+console.log(newAllBooks3);
